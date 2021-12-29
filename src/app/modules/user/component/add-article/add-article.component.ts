50070387e185879d966479286e265bd315d93181
Harsiman Singh <harsimran.sliet@gmail.com>
2021-12-30 05:00:15 +0530

    update add post

--- src/app/modules/user/component/add-article/add-article.component.ts
+++ src/app/modules/user/component/add-article/add-article.component.ts
@@ -1,12 +1,15 @@
+import Quill from 'quill';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { ApiService } from 'src/app/services/api/api.service';
-import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
-import Quill from 'quill';
-import { VideoHandler, ImageHandler, Options } from 'ngx-quill-upload';
+import { ImageHandler } from 'ngx-quill-upload';
 import { HttpClient } from '@angular/common/http';
+import BlotFormatter from 'quill-blot-formatter';
+
+
 Quill.register('modules/imageHandler', ImageHandler);
+Quill.register('modules/blotFormatter', BlotFormatter);
 
 @Component({
   selector: 'app-add-article',
@@ -18,8 +21,6 @@ export class AddArticleComponent implements OnInit {
   submit: Boolean = false;
 
   modules = {
-    // #3 Add "image" to the toolbar
-   
     imageHandler: {
       upload: async (file: any) => {
         return new Promise((resolve, reject): any => {
@@ -28,40 +29,35 @@ export class AddArticleComponent implements OnInit {
             file.type === 'image/png' ||
             file.type === 'image/jpg'
           ) {
-            // File types supported for image
             if (file.size < 1000000) {
-              // Customize file size as per requirement
-
-              // Sample API Call
               const uploadData = new FormData();
               uploadData.append('image', file, file.name);
 
               return this.http
-                .post('https://api.imgbb.com/1/upload?key=c2732b3f84b80a7e50d4adc42950daf9', uploadData)
+                .post(
+                  'https://api.imgbb.com/1/upload?key=c2732b3f84b80a7e50d4adc42950daf9',
+                  uploadData
+                )
                 .toPromise()
                 .then((result: any) => {
                   resolve(result.data.url); // RETURN IMAGE URL from response
                 })
                 .catch((error) => {
                   reject('Upload failed');
-                  // Handle error control
                   console.error('Error:', error);
                 });
             } else {
               reject('Size too large');
-              // Handle Image size large logic
             }
           } else {
             reject('Unsupported type');
-            // Handle Unsupported type logic
           }
         });
       },
     },
-    imageResize: {
-      // parchment: Quill.import('parchment'),
-      modules: ['Resize', 'DisplaySize'],
-    },
+  
+    blotFormatter: {
+    }
   };
 
   constructor(
